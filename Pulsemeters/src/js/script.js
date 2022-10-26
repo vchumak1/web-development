692f7e0967522e94bce49c1e770cf5cdde5817ff
Чумак Владимир <katya.12345>
2022-10-26 15:42:32 +0300

    tabs and feedbacks sections created

--- Pulsemeters/src/js/script.js
+++ Pulsemeters/src/js/script.js
@@ -19,29 +19,55 @@ document.querySelector('.prev').addEventListener("click", function () {
 
 
 
+document.addEventListener("DOMContentLoaded", () => {
 
-//slick slider
-$(document).ready(function () {
+
+  //slick slider
+  $(document).ready(function () {
+
     $('.carousel__inner').slick({
-        speed: 1200,
-        adaptiveHeight: true,
-        prevArrow: `<button type="button" class="slick-prev"><img src="img/slider/left_btn.svg"></button>`,
-        nextArrow: `<button type="button" class="slick-next"><img src="img/slider/right_btn.svg"></button>`,
-        responsive: [{
-            breakpoint: 992,
-            settings: {
-                dots: true,
-                arrows: false
-            }
-        }]
+      speed: 1200,
+      adaptiveHeight: true,
+      prevArrow: `<button type="button" class="slick-prev"><img src="img/slider/left_btn.svg"></button>`,
+      nextArrow: `<button type="button" class="slick-next"><img src="img/slider/right_btn.svg"></button>`,
+      responsive: [{
+        breakpoint: 992,
+        settings: {
+          dots: true,
+          arrows: false
+        }
+      }]
     });
-/*     $(".slick-initialized").css("color", "transparent");
-    $(".slick-dots").css("display","flex");
-    $(".slick-dots").css("list-style-type","none");
-    $(".slick-dots").contents("li").css("margin", "auto");
-    $("li").contents("button").css("background-color", "black");
-    $("li").contents("button").css("border-radius", "100%");
-    $("li").contents("button").css("color", "transparent"); */
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+
+
+    function toggleSlide(item) {
+      $(item).each(function (i) {
+        $(this).on('click', function (e) {
+          e.preventDefault();
+          $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+          $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        });
+      });
+    }
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+  });
+
+
+  /*     $(".slick-initialized").css("color", "transparent");
+      $(".slick-dots").css("display","flex");
+      $(".slick-dots").css("list-style-type","none");
+      $(".slick-dots").contents("li").css("margin", "auto");
+      $("li").contents("button").css("background-color", "black");
+      $("li").contents("button").css("border-radius", "100%");
+      $("li").contents("button").css("color", "transparent"); */
 });
 
 
