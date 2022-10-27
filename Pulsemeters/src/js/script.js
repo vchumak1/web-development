e11beb1e3100188387709335a9ddea945eb0bbfe
Чумак Владимир <katya.12345>
2022-10-27 14:10:57 +0300

    forms validation done

--- Pulsemeters/src/js/script.js
+++ Pulsemeters/src/js/script.js
@@ -19,9 +19,6 @@ document.querySelector('.prev').addEventListener("click", function () {
 
 
 
-document.addEventListener("DOMContentLoaded", () => {
-
-
   //slick slider
   $(document).ready(function () {
 
@@ -62,19 +59,52 @@ document.addEventListener("DOMContentLoaded", () => {
 
     //Modal
 
-    $('[data-modal=consultation]').on('click', function() {
+    $('[data-modal=consultation]').on('click', function () {
       $('.overlay, #consultation').fadeIn('slow');
     });
-    $('.modal__close').on('click', function() {
+    $('.modal__close').on('click', function () {
       $('.overlay, consultation, #thanks, #order').fadeOut('slow');
     });
 
-    $('.button_mini').each(function(i) {
-      $(this).on('click', function() {
+    $('.button_mini').each(function (i) {
+      $(this).on('click', function () {
         $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
         $('.overlay, #order').fadeIn('slow');
       });
     });
+
+
+    function validateForms (form) {
+      $(form).validate({
+        rules: {
+          name: {
+            required: true,
+            minlength: 2
+          },
+          phone: 'required',
+          email: {
+            required: true,
+            email: true
+          }
+        },
+        messages: {
+          name: {
+            required: "Пожалуйста введите своё имя",
+            minlength: jQuery.validator.format("Введите {0} символа!")
+          },
+          phone: "Пожалуйста введите свой номер телефона",
+          email: {
+            required: "Пожалуйста введите свою почту",
+            email: "Неправильно введён адрес почты"
+          }
+        }
+      });
+    }
+
+    validateForms('#consultation form');
+    validateForms('#consultation-form');
+    validateForms('#order form');
+
   });
 
 
@@ -85,6 +115,5 @@ document.addEventListener("DOMContentLoaded", () => {
       $("li").contents("button").css("background-color", "black");
       $("li").contents("button").css("border-radius", "100%");
       $("li").contents("button").css("color", "transparent"); */
-});
 
 
