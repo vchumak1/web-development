ab55d603446822a734f4fa6133a2eaa8070ca162
Чумак Владимир <katya.12345>
2022-10-27 11:54:17 +0300

    modal windows added, interactive actions programmed, modal windows optimized for mobile devices

--- Pulsemeters/src/js/script.js
+++ Pulsemeters/src/js/script.js
@@ -39,6 +39,7 @@ document.addEventListener("DOMContentLoaded", () => {
       }]
     });
 
+    //Tabs eventlistener
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
       $(this)
         .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
@@ -58,6 +59,22 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    //Modal
+
+    $('[data-modal=consultation]').on('click', function() {
+      $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+      $('.overlay, consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i) {
+      $(this).on('click', function() {
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('slow');
+      });
+    });
   });
 
 
