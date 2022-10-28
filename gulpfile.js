f810c22f7d4a8ea3b10a6e37d117073c9523c39b
Чумак Владимир <katya.12345>
2022-10-28 13:09:18 +0300

    gulp file modified

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,20 @@
-"use strict";
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
 const sass = require('gulp-sass')(require('sass'));
 const rename = require("gulp-rename");
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
 
 // Static server
 gulp.task('server', function() {
     browserSync.init({
         server: {
-            baseDir: "Pulsemeters/src"
+            baseDir: "Pulsemeters/dist"
         }
     });
+    gulp.watch("Pulsemeters/src/*.html").on("change", browserSync.reload);  
 });
 
 gulp.task('styles', function() {
@@ -26,13 +28,48 @@ gulp.task('styles', function() {
 			cascade: false
 		}))
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("Pulsemeters/src/css"))
+        .pipe(gulp.dest("Pulsemeters/dist/css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch('Pulsemeters/src/**/*.+(scss|sass)', gulp.parallel("styles"));
-    gulp.watch("Pulsemeters/src/*.html").on("change", browserSync.reload); 
+    gulp.watch('Pulsemeters/src/**/*.+(scss|sass|css)', gulp.parallel("styles"));
+    gulp.watch("Pulsemeters/src/*.html").on("change", gulp.parallel('html'));  
 });
 
-gulp.task('default', gulp.parallel('server', 'styles', 'watch'));
+gulp.task('html', function() {
+    return gulp.src("Pulsemeters/src/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('Pulsemeters/dist/'));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src("Pulsemeters/src/js/**/*.js")
+        .pipe(gulp.dest('Pulsemeters/dist/js'));
+});
+
+gulp.task('icons', function() {
+    return gulp.src("Pulsemeters/src/icons/**/*")
+        .pipe(gulp.dest('Pulsemeters/dist/icons'));
+});
+
+gulp.task('mailer', function() {
+    return gulp.src("Pulsemeters/src/mailer/**/*")
+        .pipe(gulp.dest('Pulsemeters/dist/mailer'));
+});
+
+gulp.task('images', function() {
+    return gulp.src("Pulsemeters/src/img/**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest('Pulsemeters/dist/img'));
+});
+
+gulp.task('fonts', function() {
+    return gulp.src("Pulsemeters/src/fonts/**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest('Pulsemeters/dist/fonts'));
+});
+
+
+
+gulp.task('default', gulp.parallel('server', 'styles', 'watch', 'html', 'scripts', 'icons', 'mailer', 'images', 'fonts'));
