c3725576efa37a1c9864b1b440d8ca86eea887ac
Чумак Владимир <katya.12345>
2022-11-03 15:30:52 +0300

    some features to be done

--- MyPortfolio/src/js/script.js
+++ MyPortfolio/src/js/script.js
@@ -5,7 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const hamburger = document.querySelector(".hamburger"),
         close = document.querySelector(".menu__close"),
         menu = document.querySelector(".menu"),
-        menuItems = document.querySelectorAll(".menu__list");
+        menuItems = document.querySelectorAll(".menu__list"),
+        counters = document.querySelectorAll(".tools-visual__percent"),
+        lines = document.querySelectorAll(".tools-visual__indicator span"),
+        links = document.querySelectorAll(".portfolio__item");
 
 
     hamburger.addEventListener("click", () => {
@@ -24,8 +27,27 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
-    const counters = document.querySelectorAll(".tools-visual__percent"),
-        lines = document.querySelectorAll(".tools-visual__indicator span");
+    links.forEach((item, index) => {
+            if (index > 1) {
+                item.addEventListener("mouseenter", (e) => {
+                    e.preventDefault();
+                    item.classList.toggle("active");
+                    const div = document.createElement("div");
+                   
+                    item.style.cssText = "position: relative;";
+                    div.style.cssText = "position: absolute; top: 50%; transform: translateY(-50%); font-size: 25px; left: 25%; font-weight: bold; color: black;";
+                    div.classList.add("portfolio__item-dev");
+                    div.innerHTML = `В разработке`;
+                    item.append(div);
+            
+                    item.addEventListener("mouseout", () => {
+                        item.classList.remove("active");
+                        item.style.cssText = "";
+                        div.remove();
+                    });      
+                });
+            }
+    });
 
     counters.forEach((item, i) => {
         lines[i].style.width = item.innerHTML;
