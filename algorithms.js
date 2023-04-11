e324dc275c271f528311b9859377774a720cd3c7
Vladimir <vchumak1@yandex.ru>
2023-04-11 17:48:35 +0300

    some practice tasks changed

--- algorithms.js
+++ algorithms.js
@@ -79,38 +79,10 @@ function counter(num) {
 
 console.log(counter(3));
 
-
-//пример сортировки с возвратом индекса числа
-
-function findSmallest(arr) {
-    let smallest = arr[0];
-    let smallestIndex = 0;
-    for (let i = 0; i < arr.length; i++) {
-        //если первый элемент меньше последующих то функция вернет изначально заданный индекс, который равен 0, если найдется число меньшее начального то вернется его индекс.
-        if (arr[i] < smallest) {
-            smallest = arr[i];
-            smallestIndex = i;
-        }
-    }
-    return smallestIndex;
-}
-
-function selectionSort(arr) {
-    let newArr = [];
-    for (let i = 0; i < arr.length; i++) {
-        //здесь используем функцию, которая уже нашла минимальное число и вернрула его индекс и заполняем новый массив, индексом данного числа.
-        let smallest = findSmallest(arr);
-        newArr.push(smallest);
-    }
-    return newArr;
-}
-
-console.log(selectionSort([15, 4, 6, 2, 1, 10]));
-
 //пример бинарного поиска
 let arr = [];
 
-for (let i = 1; i <= 1000000000; i++) {
+for (let i = 1; i <= 1000000; i++) {
     arr.push(i);
 }
 
@@ -130,7 +102,7 @@ function guessNumber(arr, item) {
         let guess = arr[mid];
         //сравниваем средний элемент с нашим item, если равен то ок
         if (guess == item) {
-            return mid;
+            return mid + 1;
         }
         //если загаданный элемент больше среднего значения то идем от конца массива
         if (guess > item) {
